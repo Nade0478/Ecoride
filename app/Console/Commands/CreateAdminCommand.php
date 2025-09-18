71b4a53e6e869a911647180d1f0ef41de39d81ce
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-09-18 06:45:57 +0200

    modif suite

--- app/Console/Commands/CreateAdminCommand.php
+++ app/Console/Commands/CreateAdminCommand.php
@@ -3,7 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Role;
-use App\Models\User; // <-- corriger cet import
+use App\Models\User; 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
 
